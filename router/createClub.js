2fa95fa8b5f09dd9a0c934aea9990a4cca8bd416
Dezx20 <2009awektoppo@gmail.com>
2021-12-24 17:54:25 +0530

    backend idiots

--- router/createClub.js
+++ router/createClub.js
@@ -1,12 +1,17 @@
-const express = require('express');
+const express = require("express");
 
-const authController = require('../controller/createClub');
+const authController = require("../controller/createClub");
 const router = express.Router();
-const Auth = require('../token/token')
+const Auth = require("../token/token");
 // upload Docs to the local host
-router.post('/checkClub',authController.checkClub);
-router.post('/createclub',Auth,authController.upload.single("docs"),authController.createClub)
-router.post('/joinclub',Auth,authController.joinClub)
-router.get('/findClub',authController.findClub)
-router.post('/memberList',authController.getMemberList)
+router.post("/checkClub", authController.checkClub);
+router.post(
+  "/createclub",
+  Auth,
+  authController.upload.single("docs"),
+  authController.createClub
+);
+router.post("/joinclub", Auth, authController.joinClub);
+router.get("/findClub", authController.findClub);
+router.post("/memberList", authController.getMemberList);
 module.exports = router;
