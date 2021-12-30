9a644787e79e9a0de105e0f6eb2d8a3d44ef3724
Dezx20 <2009awektoppo@gmail.com>
2021-12-30 16:19:04 +0530

    schema of signUp details edited to string

--- models/auth.js
+++ models/auth.js
@@ -1,81 +1,81 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const signUpSchema = new Schema({
-    name:{
-      type: String,
-      required: true,
-    },
-    email:{
-        type: String,
-        required: true,
-        unique: true,
-    },
-    phone:{
-        type:Number,
-        required:true
-    },
-    department:{
-        type: String,
-        required: true,
-    },
-    rollno:{
-      type: Number,
-      unique: true
-    },
-    enroll:{
-        type: String,
-        unique: true
-    },
-    year:{
-       type:Number,
-       required:true,
-    },
-    admissionYear:{
-        type: Number,
-        required: true
-    },
-    semester:{
-        type:Number,
-        required:true
-    },
-    password:{
-        type: String,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  department: {
+    type: String,
+    required: true,
+  },
+  rollno: {
+    type: String,
+    unique: true,
+  },
+  enroll: {
+    type: String,
+    unique: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  admissionYear: {
+    type: String,
+    required: true,
+  },
+  semester: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
-signUpSchema.statics.isThisEmail = async function(email){
-    try {
-        const user = await this.findOne({email})
-        if(user) return false;
+signUpSchema.statics.isThisEmail = async function (email) {
+  try {
+    const user = await this.findOne({ email });
+    if (user) return false;
 
-        return true;
-    } catch (error) {
-        console.log('error inside method ',error.message)
-        return false;
-    }
-}
-signUpSchema.statics.isThisRoll = async function(rollno){
-    try {
-        const user = await this.findOne({rollno})
-        if(user) return false;
+    return true;
+  } catch (error) {
+    console.log("error inside method ", error.message);
+    return false;
+  }
+};
+signUpSchema.statics.isThisRoll = async function (rollno) {
+  try {
+    const user = await this.findOne({ rollno });
+    if (user) return false;
 
-        return true;
-    } catch (error) {
-        console.log('error inside method ',error.message)
-        return false;
-    }
-}
-signUpSchema.statics.isThisEnroll = async function(enroll){
-    try {
-        const user = await this.findOne({enroll})
-        if(user) return false;
+    return true;
+  } catch (error) {
+    console.log("error inside method ", error.message);
+    return false;
+  }
+};
+signUpSchema.statics.isThisEnroll = async function (enroll) {
+  try {
+    const user = await this.findOne({ enroll });
+    if (user) return false;
 
-        return true;
-    } catch (error) {
-        console.log('error inside method ',error.message)
-        return false;
-    }
-}
-module.exports = mongoose.model('SignUp',signUpSchema);
+    return true;
+  } catch (error) {
+    console.log("error inside method ", error.message);
+    return false;
+  }
+};
+module.exports = mongoose.model("SignUp", signUpSchema);
